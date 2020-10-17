c9bb6bc960cb0af09d90dedd8258894fbc9751e2
botlikes <botlikes123@yandex.com>
2020-10-18 00:12:42 +0800

    mapping factor
    Signed-off-by: botlikes <botlikes123@yandex.com>

--- src/pages/mapping-factor/index.tsx
+++ src/pages/mapping-factor/index.tsx
@@ -3,7 +3,7 @@ import { useHistory } from 'react-router-dom';
 import styled from 'styled-components';
 import Path from '../../common/path';
 import { BigButton, ButtonType } from '../component/button';
-import { useGuideContext } from '../guide/guide-context';
+import { GuideData, useGuideContext } from '../guide/guide-context';
 
 const ObjectsContainer = styled.div`
 	display: flex;
@@ -14,16 +14,31 @@ const ObjectsContainer = styled.div`
 	overflow: hidden;
 `;
 const ObjectsList = styled.div`
+	position: relative;
 	width: 30%;
 	display: flex;
 	flex-direction: column;
 	border-right: var(--border);
-`;
-const ObjectDetail = styled.div`
-	
+	&[data-has-data=false] {
+		width: 100%;
+		border-right: 0;
+		& + div {
+			display: none;
+		}
+		&:before {
+			content: 'No Data Imported';
+			font-weight: var(--font-bold);
+			display: block;
+			position: absolute;
+			top: 50%;
+			left: 50%;
+			transform: translate(-50%, -50%);
+		}
+	}
 `;
 const ObjectItem = styled.div`
 	display: flex;
+	position: relative;
 	align-items: center;
 	height: 32px;
 	padding: 0 calc(var(--margin) / 2);
@@ -32,8 +47,9 @@ const ObjectItem = styled.div`
 	text-overflow: ellipsis;
 	overflow-x: hidden;
 	border-bottom: var(--border);
+	transition: all 300ms ease-in-out;
 	&:nth-child(n + 11):last-child {
-		border-bottom: transparent;
+		border-bottom-color: transparent;
 	}
 	&:hover {
 		background-color: var(--hover-color);
@@ -42,6 +58,27 @@ const ObjectItem = styled.div`
 		background-color: var(--active-color);
 		color: var(--invert-color);
 	}
+`;
+const ObjectDetail = styled.div`
+	width: 70%;
+	display: flex;
+	flex-direction: column;
+	opacity: 0;
+	transition: all 300ms ease-in-out;
+	&[data-visible=true] {
+		opacity: 1;
+	}
+`;
+const ObjectDetailHeader = styled.div`
+	display: grid;
+	grid-template-columns: 40% 35% 25%;
+	border-bottom: var(--border);
+`;
+const ObjectDetailHeaderCell = styled.div`
+	height: 31px;
+	padding: 0 calc(var(--margin) / 2);
+	display: flex;
+	align-items: center;
 `;
 const Operations = styled.div`
 	display: flex;
@@ -68,20 +105,24 @@ export default () => {
 		history.push(Path.GUIDE_MEASURE_INDICATOR);
 	};
 
-	const data = guide.getData() || { "No Data": { data: [], columns: [] } };
+	const data = (guide.getData() || {}) as GuideData;
 	const onObjectSelected = (key: string) => () => setActiveKey(key);
 
 	return <Fragment>
 		<ObjectsContainer>
-			<ObjectsList>
+			<ObjectsList data-has-data={Object.keys(data).length !== 0} data-has-active={activeKey != null}>
 				{Object.keys(data).sort((k1, k2) => k1.localeCompare(k2)).map((key, index) => {
 					return <ObjectItem key={key} onClick={onObjectSelected(key)} data-active={key === activeKey}>
 						{key}
 					</ObjectItem>;
 				})}
 			</ObjectsList>
-			<ObjectDetail>
-
+			<ObjectDetail data-visible={activeKey != null}>
+				<ObjectDetailHeader>
+					<ObjectDetailHeaderCell>Name</ObjectDetailHeaderCell>
+					<ObjectDetailHeaderCell>Label</ObjectDetailHeaderCell>
+					<ObjectDetailHeaderCell>Type</ObjectDetailHeaderCell>
+				</ObjectDetailHeader>
 			</ObjectDetail>
 		</ObjectsContainer>
 		<Operations>
