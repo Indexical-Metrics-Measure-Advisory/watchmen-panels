5a8f207bc93ac75e177105252a7352e7d55fba7d
botlikes <botlikes123@yandex.com>
2020-11-24 15:39:19 +0800

    console, admin
    Signed-off-by: botlikes <botlikes123@yandex.com>

--- src/pages/admin/topic/index.tsx
+++ src/pages/admin/topic/index.tsx
@@ -1,12 +1,38 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import styled from 'styled-components';
 import { NarrowPageTitle } from '../../component/console/narrow-page-title';
 import { PlainNarrowContainer } from '../../component/console/page-container';
+import Input from '../../component/input';
+
+const Main = styled.div`
+	display: flex;
+	flex-direction: column;
+	margin-top: 50px;
+`;
+const Search = styled(Input)`
+	border-radius: 0;
+	border-top: 0;
+	border-left: 0;
+	border-right: 0;
+	padding-left: 0;
+	padding-right: 0;
+	font-size: 1.4em;
+`;
 
 export const Topics = () => {
+	const searchRef = useRef<HTMLInputElement>(null);
+
+	useEffect(() => {
+		if (searchRef.current) {
+			searchRef.current.focus();
+		}
+	}, []);
+
 	return <PlainNarrowContainer>
 		<NarrowPageTitle title='Topics'/>
-		<div style={{ marginTop: 50, fontSize: '2em', opacity: 0.5 }}>
-			Coming soon...
-		</div>
+		<Main>
+			<Search placeholder='Search by topic name, factor name, description, etc.'
+			        ref={searchRef}/>
+		</Main>
 	</PlainNarrowContainer>;
 };
