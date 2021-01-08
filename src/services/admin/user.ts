d051f8dbb46c73641e71d4095005bc0abcee8f19
luke0623 <luke0623@outlook.com>
2021-01-08 18:13:12 +0800

    add fetch user api
    add fetch user api

--- src/services/admin/user.ts
+++ src/services/admin/user.ts
@@ -152,30 +152,49 @@ export const listUserGroupsForUser = async (search: string): Promise<Array<Queri
 export const fetchUser = async (
 	userId: string
 ): Promise<{ user: User; groups: Array<QueriedUserGroupForGroupsHolder> }> => {
-	let user;
-	switch (userId) {
-		case "1":
-			user = { userId: "1", name: "Damon Lindelof", groupIds: ["1"] };
-			break;
-		case "2":
-			user = { userId: "2", name: "Sally Jupiter", groupIds: ["1"] };
-			break;
-		case "3":
-			user = { userId: "3", name: "Roy Raymond", groupIds: ["1"] };
-			break;
-		case "4":
-			user = { userId: "4", name: "Walter Kovacs", groupIds: ["1"] };
-			break;
-		case "5":
-			user = { userId: "5", name: "Jeffrey Dean Morgan", groupIds: ["1"] };
-			break;
-		default:
-			user = {};
+	console.log(isMockService());
+	if (isMockService()) {
+		// call api
+		let user;
+		switch (userId) {
+			case "1":
+				user = { userId: "1", name: "Damon Lindelof", groupIds: ["1"] };
+				break;
+			case "2":
+				user = { userId: "2", name: "Sally Jupiter", groupIds: ["1"] };
+				break;
+			case "3":
+				user = { userId: "3", name: "Roy Raymond", groupIds: ["1"] };
+				break;
+			case "4":
+				user = { userId: "4", name: "Walter Kovacs", groupIds: ["1"] };
+				break;
+			case "5":
+				user = { userId: "5", name: "Jeffrey Dean Morgan", groupIds: ["1"] };
+				break;
+			default:
+				user = {};
+		}
+		return {
+			user,
+			groups: [{ userGroupId: "1", name: "Oklahoma", description: "Northwest market analysis squad." }],
+		};
+
+		// const token: string = Storage.findToken();
+		// const account = Storage.findAccount();
+	} else {
+		const response = await fetch(`${getServiceHost()}user?user_id=${userId}`, {
+			method: "GET",
+			headers: {
+				"Content-Type": "application/json",
+				// authorization: token,
+			},
+		});
+
+		let user = await response.json();
+		//TODO load groups
+		return { user, groups: [] };
 	}
-	return {
-		user,
-		groups: [{ userGroupId: "1", name: "Oklahoma", description: "Northwest market analysis squad." }],
-	};
 };
 
 export const saveUser = async (user: User): Promise<void> => {
@@ -206,46 +225,65 @@ export const saveUser = async (user: User): Promise<void> => {
 export const fetchUserGroup = async (
 	userGroupId: string
 ): Promise<{ group: UserGroup; users: Array<QueriedUserForUserGroup>; spaces: Array<QueriedSpaceForUserGroup> }> => {
-	let group: UserGroup;
-	switch (userGroupId) {
-		case "1":
-			group = {
-				userGroupId: "1",
-				name: "Oklahoma",
-				description: "South-center market analysis squad.",
-				userIds: ["1", "2", "3", "4", "5"],
-				spaceIds: ["1"],
-			};
-			break;
-		case "2":
-			group = { userGroupId: "2", name: "Delaware", userIds: [], spaceIds: [] };
-			break;
-		case "3":
-			group = { userGroupId: "3", name: "Hawaii", userIds: [], spaceIds: [] };
-			break;
-		case "4":
-			group = { userGroupId: "4", name: "Alaska", userIds: [], spaceIds: [] };
-			break;
-		case "5":
-			group = { userGroupId: "5", name: "Missouri", userIds: [], spaceIds: [] };
-			break;
-		case "6":
-			group = { userGroupId: "6", name: "Arkansas", userIds: [], spaceIds: [] };
-			break;
-		default:
-			group = {};
+	console.log(isMockService());
+	if (isMockService()) {
+		// call api
+		let group: UserGroup;
+		switch (userGroupId) {
+			case "1":
+				group = {
+					userGroupId: "1",
+					name: "Oklahoma",
+					description: "South-center market analysis squad.",
+					userIds: ["1", "2", "3", "4", "5"],
+					spaceIds: ["1"],
+				};
+				break;
+			case "2":
+				group = { userGroupId: "2", name: "Delaware", userIds: [], spaceIds: [] };
+				break;
+			case "3":
+				group = { userGroupId: "3", name: "Hawaii", userIds: [], spaceIds: [] };
+				break;
+			case "4":
+				group = { userGroupId: "4", name: "Alaska", userIds: [], spaceIds: [] };
+				break;
+			case "5":
+				group = { userGroupId: "5", name: "Missouri", userIds: [], spaceIds: [] };
+				break;
+			case "6":
+				group = { userGroupId: "6", name: "Arkansas", userIds: [], spaceIds: [] };
+				break;
+			default:
+				group = {};
+		}
+		return {
+			group,
+			users: [
+				{ userId: "1", name: "Damon Lindelof" },
+				{ userId: "2", name: "Sally Jupiter" },
+				{ userId: "3", name: "Roy Raymond" },
+				{ userId: "4", name: "Walter Kovacs" },
+				{ userId: "5", name: "Jeffrey Dean Morgan" },
+			],
+			spaces: [{ spaceId: "1", name: "Quotation & Policy", description: "All Sales Data" }],
+		};
+
+		// const token: string = Storage.findToken();
+		// const account = Storage.findAccount();
+	} else {
+		const response = await fetch(`${getServiceHost()}user_group?user_group_id=${userGroupId}`, {
+			method: "GET",
+			headers: {
+				"Content-Type": "application/json",
+				// authorization: token,
+			},
+		});
+
+		let group = await response.json();
+		//TODO load space and  user
+		return { group, users: [], spaces: [] };
 	}
-	return {
-		group,
-		users: [
-			{ userId: "1", name: "Damon Lindelof" },
-			{ userId: "2", name: "Sally Jupiter" },
-			{ userId: "3", name: "Roy Raymond" },
-			{ userId: "4", name: "Walter Kovacs" },
-			{ userId: "5", name: "Jeffrey Dean Morgan" },
-		],
-		spaces: [{ spaceId: "1", name: "Quotation & Policy", description: "All Sales Data" }],
-	};
 };
 
 export const saveUserGroup = async (group: UserGroup): Promise<void> => {
