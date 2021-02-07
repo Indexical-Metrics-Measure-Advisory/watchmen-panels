5c6f96b624bbd1d79e3bc85446f8945a251bc69b
skywalkerX69 <skywalkerX69@outlook.com>
2021-02-07 12:43:17 +0800

    Update service_utils.ts
    updated

--- src/services/service_utils.ts
+++ src/services/service_utils.ts
@@ -1,6 +1,6 @@
 export const isMockService = (): boolean => process.env.REACT_APP_SERVICE_MOCK_FLAG === "true";
 export const getServiceHost = (): string => {
-	if (window.location.hostname == "localhost") {
+	if (window.location.hostname === "localhost") {
 		return process.env.REACT_APP_SERVICE_URL!;
 	} else {
 		return window.location.protocol + "//" + window.location.host + "/watchmen/";
